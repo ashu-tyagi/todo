c6a3a12b7ae746730e0107f7d772850f0c6101b6
ashu <ashu.tyagi@engineer.io>
2020-08-12 09:24:09 +0530

    ui

--- src/app/form1/form1.component.ts
+++ src/app/form1/form1.component.ts
@@ -6,20 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form1.component.scss']
 })
 export class Form1Component implements OnInit {
-  constructor() { }
-  dataList = ['Phones', 'Refregetor'];
-  
-  inputData:any;
 
+  constructor() { }
+  dataList = ["a", "b"];
+  inputData:string = "";
+  currentIndex:number= -1;
   ngOnInit() {}
 
   addData(){
-    // console.log(this.inputData);
     if(this.inputData.length > 0){
-      console.log(this.inputData);
+      //console.log(this.inputData);
       this.dataList.push(this.inputData);
       this.inputData = "";
     } 
+  }
+  remove(i){
+    this.dataList.splice(i, 1);
+  }
+  edit(i){
+    this.currentIndex = i;
+    this.inputData = this.dataList[i];
+  }
+  update(){
+    this.dataList[this.currentIndex] = this.inputData;
+    this.currentIndex = -1;
+    this.inputData = "";
   }
 
 }
